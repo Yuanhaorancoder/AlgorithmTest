91549f3e4b095dc1a6b89b3e446a7f8855cb6044
Yuanhaorancoder <937558016@qq.com>
2025-11-25 23:37:31 +0800

    位运算专题

---
+++ 268.丢失的数字（位运算）.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.13.35931.197 d17.13
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "268.丢失的数字（位运算）", "268.丢失的数字（位运算）\268.丢失的数字（位运算）.vcxproj", "{1C83766E-A51E-45E7-A10E-343DE900B4C1}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Debug|x64.ActiveCfg = Debug|x64
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Debug|x64.Build.0 = Debug|x64
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Debug|x86.ActiveCfg = Debug|Win32
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Debug|x86.Build.0 = Debug|Win32
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Release|x64.ActiveCfg = Release|x64
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Release|x64.Build.0 = Release|x64
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Release|x86.ActiveCfg = Release|Win32
+		{1C83766E-A51E-45E7-A10E-343DE900B4C1}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {E261C2C9-004B-4569-B822-D7F545D05994}
+	EndGlobalSection
+EndGlobal
